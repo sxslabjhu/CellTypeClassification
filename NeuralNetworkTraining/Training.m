36c27225002207cbebd02467e0790425b6c88867
sxslabjhu <46148707+sxslabjhu@users.noreply.github.com>
2019-03-04 16:35:02 -0500

    Update Training.m

--- NeuralNetworkTraining/Training.m
+++ NeuralNetworkTraining/Training.m
@@ -3,7 +3,6 @@ clear all;
 close all
 
 % Preparation
-
 numTrainPercentage = 0.9;
 parentFile = cd;
 dsFile = cd;
@@ -12,7 +11,6 @@ imds = imageDatastore(dsFile,'IncludeSubfolders',true,'LabelSource','foldernames
 count = countEachLabel(imds);
 
 % Set Up Training Options
-
 [imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainPercentage,'randomize');
 
 layers = [
@@ -70,7 +68,6 @@ save('net','net'); % this provides you with a 'net.mat' file you can then use to
 save('info','info');
 
 % Compute Accuracy of Validation Set
-
 YPred = classify(net,imdsValidation);
 YValidation = imdsValidation.Labels;
 accuracy = sum(YPred == YValidation)/numel(YValidation);
